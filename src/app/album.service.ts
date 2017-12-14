450130320d7559d3f3e067313987ad9a89d22d0b
Valia Dempsey <valia@>
2017-12-14 10:46:08 -0800

    Define deleteAlbum method in album.service.ts

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -30,4 +30,9 @@ export class AlbumService {
                                  description: localUpdatedAlbum.description});
   }
 
+  deleteAlbum(localAlbumToDelete) {
+    let albumEntryInFirebase = this.getAlbumById(localAlbumToDelete.$key);
+    albumEntryInFirebase.remove();
+  }
+
 }
